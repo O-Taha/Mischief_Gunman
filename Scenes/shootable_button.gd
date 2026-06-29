291890d1910db3e075cad0ce8447af0fedebfabe
O-Taha <Taha.Ouerfili@etu.sorbonne-universite.fr>
2026-06-29 20:48:58 +0200

    feat: Changed shootable button with label + autosets collision shape

--- Scenes/shootable_button.gd
+++ Scenes/shootable_button.gd
@@ -1,8 +1,18 @@
-extends ColorRect
+@tool
+extends RichTextLabel
 var function: Callable
 
+@export_category("Nodes & Scenes")
+@export var collision: RigidBody2D
+@export var collision_shape: CollisionShape2D
+
 func _ready() -> void:
-	$ButtonCollision.hit.connect(_on_hit)
+	if not Engine.is_editor_hint(): collision.hit.connect(_on_hit)
+	
+	var button_shape = RectangleShape2D.new()
+	button_shape.size = size
+	collision_shape.shape = button_shape
+	collision.position = size/2
 	
 func _on_hit():
 	function.call()
